c4bd04e6d6ba1ef23803d72e1bd94bfe3eea7039
AranoYuki1 <aranoyuki1@gmail.com>
2023-07-28 12:30:02 +0900

    support firefox

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,11 +1,10 @@
 import browser from 'webextension-polyfill';
-import { Image, PostOptions, PostMessage } from "../common/CommonType"
+import { PostMessage } from "../common/CommonType"
 import { postToMisskey } from "./MisskeyAPI"
 
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type == 'post') {
     const postMessage = message as PostMessage
-    console.log(postMessage)
-    postToMisskey(postMessage.text, postMessage.images, postMessage.options)
+    return postToMisskey(postMessage.text, postMessage.images, postMessage.options)
   }
 });
