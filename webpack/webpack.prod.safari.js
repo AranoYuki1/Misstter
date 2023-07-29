a02a4f39f4109fb70b6f2f1da7c6d1fb303b305d
AranoYuki1 <aranoyuki1@gmail.com>
2023-07-29 13:02:49 +0900

    add icons

--- webpack/webpack.prod.safari.js
+++ webpack/webpack.prod.safari.js
@@ -4,10 +4,7 @@ const common = require('./webpack.common.js');
 const defaultConfig = common('safari', 'production');
 
 defaultConfig.plugins.push(
-  new CopyPlugin({
-    patterns: [{ from: "./icons/", to: "../icons/", context: `browser/safari` }],
-    options: {},
-  }),
+  
 );
 
 module.exports = defaultConfig
