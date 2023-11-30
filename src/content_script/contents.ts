cad2fadb8cd5bd837cb176c8f49a122c39f5a72c
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-11-30 09:29:25 +0900

    fix: #6 TweetdeckのURL変更

--- src/content_script/contents.ts
+++ src/content_script/contents.ts
@@ -2,7 +2,7 @@ if (location.hostname === 'twitter.com') {
   // is current location is twitter.com load twitter content script
   import('./Clients/twitter');
 
-} else if (location.hostname === "tweetdeck.twitter.com") {
-  // is current location is tweetdeck.twitter.com load tweetdeck content script
+} else if (location.hostname === "pro.twitter.com") {
+  // is current location is pro.twitter.com load tweetdeck content script
   import('./Clients/tweetdeck');
 } 
