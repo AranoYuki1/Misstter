fcbcdba40c58d6d0423275847f30677858ef13d1
hierv5 <hierv5@outlook.jp>
2024-05-18 01:25:02 +0900

    feature: Twitter→X ドメイン移行への対応
    - x.comへのドメイン移行後でもMisskey投稿ボタンが表示されるように記述を追加

--- src/content_script/contents.ts
+++ src/content_script/contents.ts
@@ -1,8 +1,10 @@
-if (location.hostname === 'twitter.com') {
+if (location.hostname === 'twitter.com' ||
+  location.hostname === 'x.com') {
   // is current location is twitter.com load twitter content script
   import('./Clients/twitter');
 
-} else if (location.hostname === "tweetdeck.twitter.com") {
+} else if (location.hostname === 'tweetdeck.twitter.com' ||
+  location.hostname === 'tweetdeck.x.com') {
   // is current location is tweetdeck.twitter.com load tweetdeck content script
   import('./Clients/tweetdeck');
 } 
